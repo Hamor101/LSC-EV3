ad4568406f1d95059dbe88f40e17d33f74764c30
Bence <Hellopeter60@gmail.com>
2025-01-11 10:50:43 +0100

    Arm control finished

--- Lesson9/robot_arm.py
+++ Lesson9/robot_arm.py
@@ -1,3 +1,7 @@
+import logging
+import threading
+
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (
     Motor,
@@ -5,6 +9,9 @@ from pybricks.ev3devices import (
 )
 from pybricks.parameters import Port, Direction, Button
 from pybricks.tools import wait
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 ev3 = EV3Brick()
 motor_turn = Motor(
@@ -26,64 +33,72 @@ motor_grip = Motor(
 sensor_turn = TouchSensor(port=Port.S1)
 sensor_raise = TouchSensor(port=Port.S3)
 
-def calibrate():
-    motor_turn.run(30)
-    while not sensor_turn.pressed():
-        wait(50)
-    motor_turn.hold()
-    motor_turn.reset_angle(180)
-    motor_turn.run_angle(-45, 90)
+calibrated_motors = []
 
+
+def calibrate(m : Motor, end_crit, resetAngle, finalAngle):
+    m.run(60)
+    while not end_crit():
+        wait(50)
+        
+    m.hold()
+    m.reset_angle(resetAngle)
+    m.run_target(-90, finalAngle)
+    calibrated_motors.append(m)
 
 def manual_control():
-    spd = 120
-    spdv = 90
-    v_grip = 20
-    motor_grip.run_until_stalled(speed=v_grip)
-    motor_grip.run_angle(-v_grip, 90)
-    grip_closed = False
-    while True:
-        pressed = ev3.buttons.pressed()
-        if not pressed:
-            motor_turn.hold()
-            motor_raise.hold()
-        if len(pressed) != 1:
-            continue
-        b = pressed[0]
-        if b == Button.LEFT:
-            motor_turn.run(-spd)
-        elif b == Button.RIGHT:
-            motor_turn.run(spd)
-        elif b == Button.UP:
-            motor_raise.run(spdv)
-        elif b == Button.DOWN:
-            motor_raise.run(-spdv)
-        elif b == Button.CENTER:
-            motor_grip.run_until_stalled(speed=v_grip) ## close
-            motor_grip.run_angle(-v_grip, 90) ## open
-    wait(50)
-        
-    # print(pressed) --> [BUTTON.LEFT, BUTTON.RIGHT]
-
-def manu2():
-    v = 90
+    v = 45
     bstate = {
         Button.LEFT: False,
         Button.RIGHT: False,
         Button.UP: False,
         Button.DOWN: False
     }
+
+    grip_dir = 1
     while True:
         bp = ev3.buttons.pressed()
         for i in bstate:
             bstate[i] = i in bp
-        vt = v * (bstate[Button.LEFT] - bstate[Button.RIGHT])
+        vt = v * (bstate[Button.RIGHT] - bstate[Button.LEFT])
         vr = v * (bstate[Button.UP] - bstate[Button.DOWN])
-        motor_turn.run(vt)
-        motor_raise.run(vr)
+        if not (sensor_turn.pressed() and vt > 0):
+            motor_turn.run(vt)
+        if not (sensor_raise.pressed() and vr > 0):
+            motor_raise.run(vr)
+        
+        grip_on = Button.CENTER in bp
+        vg = v * grip_on * grip_dir
+
+        motor_grip.run(vg)
+        if motor_grip.stalled() and vg > 0 or motor_grip.angle() < -90 and vg <0:
+            grip_dir *= -1
+        
+        vs = (vt, vr, vg)
+
+        if any(vs):
+            logger.debug('Turn: %s, Raise: %s, Grip: %s', motor_turn.angle(), motor_raise.angle(), motor_grip.angle())
+        wait(20)
+
+
+
 
 def main():
-    manu2()
+    logger.info('Starting')
+    #calibrate(motor_turn, sensor_turn.pressed, 183, 90)
+    #calibrate(motor_raise, sensor_raise.pressed, 45, 0)
+    #calibrate(motor_grip, motor_grip.stalled, 0, -90)
+    th = threading.Thread(target=calibrate, args =(motor_turn, sensor_turn.pressed, 183, 90))
+    th.start()
+    th2 = threading.Thread(target = calibrate, args = (motor_raise, sensor_raise.pressed, 45, 0))
+    th2.start()
+    th3 = threading.Thread(target = calibrate, args = (motor_grip, motor_grip.stalled, 0, -90))
+    th3.start()
+    while len(calibrated_motors) < 3:
+        wait(1000)
+        print(len(calibrated_motors), end='\r')
+
+    manual_control()
     
 
 if __name__ == "__main__":
